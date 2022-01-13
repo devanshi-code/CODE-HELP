0c8b21a615c839fbb45a3d85dbb8347722cd245d
devanshi-code <devu2732@gmail.com>
2022-01-13 23:25:03 +0530

    commit

---
+++ LECTURE 4/Pattern16.cpp
@@ -0,0 +1,25 @@
+/* A
+   B C
+   D E F
+   G H I J */
+
+  #include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    char value='A';
+    int row=1;
+    while(row<=n){
+      int col=1;
+      while(col<=row){
+        
+        cout<<value;
+        value++;
+        col++;
+    }
+    cout<<endl;
+    row++;
+  }
+ return 0;
+} 
