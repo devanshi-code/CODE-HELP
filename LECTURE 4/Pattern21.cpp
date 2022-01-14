136bc34e49031763970816560d1ab858e0575cb7
devanshi-code <devu2732@gmail.com>
2022-01-15 00:22:36 +0530

    commit

--- LECTURE 4/Pattern21.cpp
+++ LECTURE 4/Pattern21.cpp
@@ -2,3 +2,28 @@
      * * *
        * * 
          *    */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  int row=1;
+  while(row<=n){
+    int space=row-1;
+    while(space){
+             cout<<" ";
+             space=space-1;
+    }
+    int col=4;
+    while(col>=row){
+        cout<<"*";
+        col--;
+    }
+    cout<<endl;
+    row++;
+       
+  }
+  return 0;
+}
