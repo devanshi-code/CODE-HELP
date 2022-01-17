1a4346629925992574e61e2feedfd8a556b0350d
devanshi-code <devu2732@gmail.com>
2022-01-18 00:22:51 +0530

    commit

---
+++ Lecture 5/Fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=10;
+    int a=0;
+    int b=1;
+    cout<<a<<" "<<b<<" ";
+    for(int i=1;i<=10;i++){
+        int next=a+b;
+        cout<<next<<" ";
+        a=b;
+        b=next;
+    }
+}
+//output  0 1 1 2 3 5 8 13 21 34 55 89
