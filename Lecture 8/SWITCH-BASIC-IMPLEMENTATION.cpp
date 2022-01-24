0a39bb1e750535da6f8fd4b0c6171ab2bc67753d
devanshi-code <devu2732@gmail.com>
2022-01-24 23:01:47 +0530

    commit

---
+++ Lecture 8/SWITCH-BASIC-IMPLEMENTATION.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=2;
+    cout<<endl;
+
+    switch (n)
+    {
+    case 1:cout<<"First"<<endl;
+           break;
+
+    case 2: cout<<"Second"<<endl;
+            break;     
+    
+    default:cout<<"It is default case"<<endl;
+            break;
+    }
+}
